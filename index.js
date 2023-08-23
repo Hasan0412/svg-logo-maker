739606907dfd482b9ec233651bb1ddeb3118860d
Hasan Malik <hmalik0412@gmail.com>
2023-08-23 04:26:23 +0100

    svg template defined and function to create logo

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const fs = require('fs');
 const { getLogoColor } = require('./lib/colorHandler');
 const { getLogoText } = require('./lib/textHandler');
 const { circle, triangle, square } = require('./lib/shape');
+
+// Define the SVG template with a placeholder for shapes and text
+const svgTemplate =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">\n' +
+  '  <!-- Add shapes and text here -->\n' +
+  '</svg>';
+
+// Function to create the logo
+async function createLogo() {
+  // Get logo text and color from user input
+  const logoText = await getLogoText();
+  const textColor = await getLogoColor();
+}
