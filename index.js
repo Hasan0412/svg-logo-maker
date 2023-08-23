ef6da556986c43fb89691d83ddf99da9eaf830fc
Hasan Malik <hmalik0412@gmail.com>
2023-08-23 04:18:41 +0100

    Imported modules

---
+++ index.js
@@ -0,0 +1,5 @@
+// Import required modules
+const fs = require('fs');
+const { getLogoColor } = require('./lib/colorHandler');
+const { getLogoText } = require('./lib/textHandler');
+const { circle, triangle, square } = require('./lib/shape');
