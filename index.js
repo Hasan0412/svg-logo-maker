4a69bce8f962586d80da1714aa6b7d83b85e6c04
Hasan Malik <hmalik0412@gmail.com>
2023-08-28 08:28:05 +0100

    es6 syntax

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Import required modules
-const fs = require('fs');
-const { getLogoColor } = require('./lib/colorHandler');
-const { circle, triangle, square } = require('./lib/shape');
-const {
+import fs from 'fs';
+import { getLogoColor } from './lib/colorHandler.js';
+import { circle, triangle, square } from './lib/shape.js';
+import {
     getLogoTextQuery,
     getShapeChoiceQuery,
     getShapeColorQuery,
     getTextColorQuery
-} = require('./lib/query');
+} from './lib/query.js';
 
 // Define the SVG template with a placeholder for shapes and text
 const svgTemplate =
@@ -15,9 +15,9 @@ const svgTemplate =
   '  <!-- Add shapes and text here -->\n' +
   '</svg>';
 
-// Function to create the logo
+  // Function to create the logo
 async function createLogo() {
-  // Get logo text and color from user input
+    // Get logo text and color from user input
   const logoText = await getLogoTextQuery();
   const textColor = await getLogoColor();
 
@@ -25,30 +25,30 @@ async function createLogo() {
   const selectedShape = await getShapeChoiceQuery(shapeOptions);
   const shapeColor = await getShapeColorQuery();
 
- // Generate SVG code for the selected shape
- let shapeSvg = '';
- switch (selectedShape) {
-   case 'circle':
-     shapeSvg = circle(shapeColor);
-     break;
-   case 'triangle':
-     shapeSvg = triangle(shapeColor);
-     break;
-   case 'square':
-     shapeSvg = square(shapeColor);
-     break;
-   default:
-     break;
- }
+   // Generate SVG code for the selected shape
+  let shapeSvg = '';
+  switch (selectedShape) {
+    case 'circle':
+      shapeSvg = circle(shapeColor);
+      break;
+    case 'triangle':
+      shapeSvg = triangle(shapeColor);
+      break;
+    case 'square':
+      shapeSvg = square(shapeColor);
+      break;
+    default:
+      break;
+  }
 
-  // Replace the placeholder in the SVG template with the logo text and shape SVG
+ // Replace the placeholder in the SVG template with the logo text and shape SVG
   const logoContent =
     svgTemplate.replace('<!-- Add shapes and text here -->', '\n' +
-      '    <text x="150" y="30" fill="' + textColor + '" font-size="16" text-anchor="middle">' + logoText + '</text>\n' +
-      '    ' + shapeSvg + '\n');
+      `    <text x="150" y="30" fill="${textColor}" font-size="16" text-anchor="middle">${logoText}</text>\n` +
+      `    ${shapeSvg}\n`);
 
-  // Write the logo content to a file named 'logo.svg'
-  fs.writeFileSync('logo.svg', logoContent);
+   // Write the logo content to a file named 'logo.svg'
+      fs.writeFileSync('logo.svg', logoContent);
   console.log('Generated logo.svg');
 }
 
